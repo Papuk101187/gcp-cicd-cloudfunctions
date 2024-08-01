935bb6c625a0aa95395b7aec810219807c3fc87d
papukoleg <42898446+Papuk101187@users.noreply.github.com>
2024-08-01 14:00:26 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const functions = require('@google-cloud/functions-framework');
 
 functions.http('helloHttp', (req, res) => {
-  res.send(`Hello ${req.query.name || req.body.name || 'World STAG'}!`);
+  res.send('Hello World');
 });
+
